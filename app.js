7c9fb04ced174a65f4afb33bea4a8fe73baaadda
Helen Oliveira <65935692+helensouz@users.noreply.github.com>
2022-01-07 01:07:19 -0300

    alterações de funcionalidades

---
+++ app.js
@@ -0,0 +1,12 @@
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){
+        setTimeout(function(){
+        elemento.innerHTML += letra;
+        }, 200 * i)
+    });
+}
+
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
