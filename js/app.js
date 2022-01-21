0a9602c83c46c40d71a0ce1276c1975eb06b0cab
Helen Oliveira <65935692+helensouz@users.noreply.github.com>
2022-01-21 12:52:44 -0300

    inserindo animações

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
-function typeWrite(elemento){
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){
-        setTimeout(function(){
-        elemento.innerHTML += letra;
-        }, 200 * i)
-    });
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach((element) => {
+        if(windowTop > element.offsetTop){
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    })
 }
+
+animeScroll()
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
+
+
+// function typeWrite(elemento){
+//     const textoArray = elemento.innerHTML.split('');
+//     elemento.innerHTML = ' ';
+//     textoArray.forEach(function(letra, i){
+//         setTimeout(function(){
+//         elemento.innerHTML += letra;
+//         }, 200 * i)
+//     });
+// }
+
+
+// typeWrite(titulo);
